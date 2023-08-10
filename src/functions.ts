81ea935074dce50a11f467853c5d41aae1189f9f
lucaslangsch <lucas.langsch@gmail.com>
2023-08-10 16:00:02 -0300

    feat: add function buildCartItem

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
+import { CartItem } from './types/CartItem';
+import { Drink } from './types/Drink';
+import { Food } from './types/Food';
+
 export function first<Type>(arr: Type[]): Type {
   return arr[0];
 }
@@ -5,4 +9,9 @@ export function first<Type>(arr: Type[]): Type {
 export function updateItem<Type>(arr: Type[], index: number, item: Type): Type[] {
   if (index >= 0 && index < arr.length) arr.splice(index, 1, item);
   return arr;
+}
+
+export function buildCartItem(item: (Food | Drink), quantity: number): CartItem {
+  const { name, price } = item;
+  return { name, price, quantity } as CartItem;
 }
