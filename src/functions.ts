9681aaf68fa894d16010ef7fd60ddebb172c4892
lucaslangsch <lucas.langsch@gmail.com>
2023-08-10 15:52:42 -0300

    feat: add function updateItem

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,8 @@
 export function first<Type>(arr: Type[]): Type {
   return arr[0];
 }
+
+export function updateItem<Type>(arr: Type[], index: number, item: Type): Type[] {
+  if (index >= 0 && index < arr.length) arr.splice(index, 1, item);
+  return arr;
+}
